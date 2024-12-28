5d194b6e10a49ee3da3a4f1aa754dd4c14c0fae6
Webprojon <tokhirjonkhasanov@gmail.com>
2024-12-28 08:21:07 +0100

    webhook is added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,29 @@
+//import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+//const isProtectedRoute = createRouteMatcher(["/settings(.*)", "/"]);
+
+//export default clerkMiddleware((auth, req) => {
+//	if (isProtectedRoute(req)) auth.protect();
+//});
+
+//export const config = {
+//	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+//};
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/settings(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-	if (isProtectedRoute(req)) auth.protect();
+export default clerkMiddleware(async (auth, req) => {
+	const authObj = await auth();
+
+	if (isProtectedRoute(req)) {
+		if (!authObj.userId) {
+			return new Response("Unauthorized", { status: 401 });
+		}
+	}
 });
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
