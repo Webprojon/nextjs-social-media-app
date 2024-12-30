e06004660c8ab75fc0d843dca834322732454fc9
Webprojon <tokhirjonkhasanov@gmail.com>
2024-12-30 09:17:40 +0100

    fix: solving

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -10,9 +10,9 @@ import React from "react";
 export default async function ProfilePage({
 	params,
 }: {
-	params: { username: string };
-}): Promise<string> {
-	const { username } = params;
+	params: Promise<{ username: string }>;
+}) {
+	const { username } = await params;
 
 	const user = await prisma.user.findFirst({
 		where: {
