c2c9e202138c9d978138f955af77ee375f53f218
Webprojon <tokhirjonkhasanov@gmail.com>
2025-01-02 09:48:45 +0100

    UserInfoCardInteraction is updated

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { switchFollow } from "@/lib/actions";
-import React, { useState } from "react";
+import { switchBlock, switchFollow } from "@/lib/actions";
+import { useOptimistic, useState } from "react";
 
-export default function UserInfoCardInteraction({
+const UserInfoCardInteraction = ({
 	userId,
 	isUserBlocked,
 	isFollowing,
@@ -13,7 +13,7 @@ export default function UserInfoCardInteraction({
 	isUserBlocked: boolean;
 	isFollowing: boolean;
 	isFollowingSent: boolean;
-}) {
+}) => {
 	const [userState, setUserState] = useState({
 		following: isFollowing,
 		blocked: isUserBlocked,
@@ -21,36 +21,65 @@ export default function UserInfoCardInteraction({
 	});
 
 	const follow = async () => {
+		switchOptimisticState("follow");
 		try {
-			// Pass a valid payload object
 			await switchFollow(userId);
 			setUserState((prev) => ({
 				...prev,
-				following: prev.following ? false : true,
-				followingRequestSent: !prev.following && !prev.followingRequestSent,
+				following: prev.following && false,
+				followingRequestSent:
+					!prev.following && !prev.followingRequestSent ? true : false,
 			}));
 		} catch (err) {
-			console.error(err);
+			console.log(err);
 		}
 	};
 
+	const block = async () => {
+		switchOptimisticState("block");
+		try {
+			await switchBlock(userId);
+			setUserState((prev) => ({
+				...prev,
+				blocked: !prev.blocked,
+			}));
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const [optimisticState, switchOptimisticState] = useOptimistic(
+		userState,
+		(state, value: "follow" | "block") =>
+			value === "follow"
+				? {
+						...state,
+						following: state.following && false,
+						followingRequestSent:
+							!state.following && !state.followingRequestSent ? true : false,
+				  }
+				: { ...state, blocked: !state.blocked },
+	);
 	return (
 		<>
 			<form action={follow}>
-				<button className="w-full bg-blue-500 hover:bg-blue-600 transition-all font-medium text-white text-sm rounded-md p-2">
-					{userState.following
+				<button className="w-full bg-blue-500 text-white text-sm rounded-md p-2">
+					{optimisticState.following
 						? "Following"
-						: userState.followingRequestSent
+						: optimisticState.followingRequestSent
 						? "Friend Request Sent"
 						: "Follow"}
 				</button>
 			</form>
-
-			<form action="" className="self-end">
-				<button className="font-medium cursor-pointer text-xs text-red-400">
-					{userState.blocked ? "Unblock User" : "Block User"}
+			<form action={block} className="self-end ">
+				<button>
+					<span className="text-red-400 text-xs cursor-pointer">
+						{optimisticState.blocked ? "Unblock User" : "Block User"}
+					</span>
 				</button>
 			</form>
 		</>
 	);
-}
+};
+
+export default UserInfoCardInteraction;
